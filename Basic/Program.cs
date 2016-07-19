adbefbf6621ebc513a958f10a901c6a2ed6074c9
till <t.amon@sae.edu>
2016-07-19 20:17:10 +0200

    doing

--- Basic/Program.cs
+++ Basic/Program.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Basic
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine(String.Join(" :: " ,  args ) );
+            Thread.Sleep(1000); 
+            Console.WriteLine( "foo " + String.Join(" :: " ,  args ) );
+
+            Thread.Sleep(3000);
+            return 1; 
         }
     }
 }
